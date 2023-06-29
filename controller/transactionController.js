263e8b15d07ddcd7a24455ab9996c218bf4f3879
Alekzandr-S <alekzandrs@outlook.com>
2023-06-29 10:02:28 +0200

    add api gateway code

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -1,6 +1,8 @@
+const pool = require('../config/db');
+const axios = require('axios')
 async function transaction(req, res) {
     try {
-        const { phone_number, amount } = req.body;
+        const { userId, phone_number, amount } = req.body;
 
         // Validate required fields
         if (!phone_number || phone_number.length !== 10) {
@@ -11,14 +13,29 @@ async function transaction(req, res) {
         if (!amount) {
             return res.status(400).json({ message: 'Enter a valid amount'})
         }
+
+        //Format amount
+        const formattedAmount = parseFloat(amount).toFixed(2);
+
         // Extract MNO from phone number
         const mnoPrefix = phone_number.substring(0,3);
         if(mnoPrefix !== '097' && mnoPrefix !== '077' && mnoPrefix !== '096' && mnoPrefix !== '066' && mnoPrefix !== '095'){
             res.status(400).json({ message: 'Invalid phone number'})
         }
 
-        //create service provider
+        //Slice and prefix phone number
+        let modifiedNumber = phone_number;
+        if (phone_number.startsWith('097') || phone_number.startsWith('077')) {
+            modifiedNumber = phone_number.slice(1);
+        }else if (phone_number.startsWith('096') || phone_number.startsWith('095')) {
+            modifiedNumber = `26${phone_number}`;
+        }
+
+        //Assign service provider to mno
+        //Determine the API endpoint based on MNO code
         let serviceProvider = '';
+        let acknowledge = '';
+        let apiEndPoint;
         switch (mnoPrefix) {
             case '097' || '077' :
                 serviceProvider = 'airtel';
@@ -29,31 +46,37 @@ async function transaction(req, res) {
             case '095':
                 serviceProvider = 'zamtel';
                 break;
-        }
-
-        let modifiedNumber = phone_number;
-
-        //Slice and prefix phone number
-        if (phone_number.startsWith('097') || phone_number.startsWith('077')) {
-            modifiedNumber = phone_number.slice(1);
-        }else if (phone_number.startsWith('096') || phone_number.startsWith('095')) {
-            modifiedNumber = `26${phone_number}`;
+            default:
+                return res.status(400).json({message: 'Invalid MNO code'})
         }
 
 
         // Consume another API gateway based on MNO
-        // axios
-        //     .post(`http://api-gateway-${mno}.com/transaction`, phone_number)
-        //     .then((response) => {
-        //         //response for api gateway
-        //         res.json({message:"Appropriate response "});
-        //     })
-        //     .catch((error) => {
-        //         //errors from API gateway
-        //         res.json(error);
-        //     });
+        axios
+            .post(`http://api-gateway-${mno}.com/transaction`, phone_number)
+            .then((response) => {
+                //response for api gateway
+                res.json({message:"Appropriate response "});
+            })
+            .catch((error) => {
+                //errors from API gateway
+                res.json(error);
+            });
 
-        res.json({ message: `Transaction initiated for ${phone_number} => ${modifiedNumber}` });
+        //
+        await pool.query(
+            'INSERT INTO transactions (userId, formattedAmount) VALUES' +
+            '(?, ?) ON DUPLICATE KEY UPDATE userId = ?, amount = ?',
+            [userId, formattedAmount]
+        );
+
+        const responsePayload = {
+            message: 'Transaction successful',
+            formattedAmount,
+            modifiedNumber
+        }
+
+        res.json(responsePayload);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
